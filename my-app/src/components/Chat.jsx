7f1a7bd49cf985c1a0c0e05b5ef5f0d8f6cbc3f1
themishkaCoder <45307191+themishkaCoder@users.noreply.github.com>
2019-04-13 09:09:28 +0300

    added dialogs

--- my-app/src/components/Chat.jsx
+++ my-app/src/components/Chat.jsx
@@ -4,16 +4,28 @@ import "./css/Chat.scss"
 const Chat = ()=>{
     return (
         <div className="content">
-            <div className="chat">
-                <div className="chat-container">
-                    <ul className="chat-area">
-                        <li>Hello</li>
-                        <li>Hi</li>
-                        <li>How are you?</li>
+            <div className="messageWrapper">
+
+                <div className="chat">
+                    <div className="chat-container">
+                        <ul className="chat-area">
+                            <li>Hello</li>
+                            <li>Hi</li>
+                            <li>How are you?</li>
+                        </ul>
+                    </div>
+                    <textarea name="" id="" cols="30" rows="10" placeholder="Write your message"></textarea>
+                    <button>Send</button>
+                </div>
+
+                <div className="dialogs">
+                    <h3>Dialogs</h3>
+                    <ul className="usersList">
+                        <li>Vasya</li>
+                        <li>Nikita</li>
+                        <li>John</li>
                     </ul>
                 </div>
-                <textarea name="" id="" cols="30" rows="10" placeholder="Write your message"></textarea>
-                <button>Send</button>
             </div>
         </div>
         
